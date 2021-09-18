7335bb76037a7ba0414c8c0e84fa81822c414210
Vlad <vladgm28@gmail.com>
2021-09-18 14:38:00 +0300

    create layouts

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->centralwidget->setLayout(ui->verticalLayout_4);
+
+//    adjustSize();
 
     database = QSqlDatabase::addDatabase("QSQLITE");
     database.setDatabaseName("./testDB.db");
